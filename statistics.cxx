f7e048d5f55c938d97a984a2ec06663e37e6b21f
Matthias <matthias@linux-xoxy.site>
2015-11-11 21:29:39 +0100

    Homework3

--- statistics.cxx
+++ statistics.cxx
@@ -3,16 +3,35 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+ 
+void random(double* x, const int N){
+  for(int i=0; i<N; i++){
+    int rand();
+    x[i]=rand()/ double(RAND_MAX);
+  }
+}
+void calc(double* x, double& mean, double& var, const int N){
+  mean=0;
+  var=0;
+  for(int i=0; i<N; i++)
+    mean=mean + x[i];
+  mean=mean/N;
+  for(int i=0; i<N; i++)
+    var= var + pow((x[i]-mean),2);
+  var=var/N;
+}  
+  
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
 
-   // Some lines here....
+   random(p,N);
+   calc(p,mean,var,N);
+   
+   //for(int i=0; i<N; i++)     just checked my random numbers
+   //  cout << p[i] << endl;
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
